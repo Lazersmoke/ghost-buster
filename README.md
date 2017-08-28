ee341e87f95e182448781b94958fc45cae4f5f89
Sam Quinn <lazersmoke@gmail.com>
2017-08-27 20:21:32 -0500

    Cleanup for hackage

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 Ghost Buster
 ============
 
-Who ya gonna call?
+This package provides an easier way to work with existential data types. 
+Normally, GHC will reject types with deeply nested `forall`s in them.
+This package uses a data contructor to hide that away, so you can nest as deeply as you like.
+
+Example code is in `src/Data/SuchThat/Example.hs`.
--- src/Data/SuchThat.hs
+++ src/Data/SuchThat.hs
@@ -9,44 +9,72 @@
 {-# LANGUAGE LiberalTypeSynonyms #-}
 module Data.SuchThat
   (SuchThat(..)
-  ,SuchThatStar
   ,ForAny
   ,Some
   ,ambiguate
   ,ambiguously
+  ,some
+  ,naturalTransformation
+  ,discoverStructure
   ) where
 
 import Data.Functor.Identity
 import GHC.Exts (Constraint)
+import Control.Monad (void)
 
+-- Ideally, we would write:
+--
+--   type SuchThat (c :: [k -> Constraint]) (p :: k -> *) = forall s. Constrain c s => p s
+-- 
+-- But type synonyms don't work that way
+
+-- | A @SuchThat c p@ is a @p s@, where @s@ is such that it satisfies all of the constraints in @c@.
+-- Note that you must always wrap results from pattern-matching on the @SuchThat@ constructor in a @SuchThat@, or GHC will complain about @s@ escaping its scope.
 data SuchThat (c :: [k -> Constraint]) (p :: k -> *) = forall s. Constrain c s => SuchThat (p s)
 
--- This should be `type SuchThatStar c = SuchThat c Id`, but Id is partially applied then
---data SuchThatStar (c :: [* -> Constraint]) = forall s. Constrain c s => SuchThatStar s
-type SuchThatStar c = SuchThat c Identity
+-- | Inject a value into a SuchThat. It becomes more ambiguous because it throws out the exact type of s.
+ambiguate :: Constrain c s => p s -> SuchThat c p
+ambiguate = SuchThat
 
--- Ideally:
---type SuchThat (c :: [k -> Constraint]) (p :: k -> *) = forall s. Constrain c s => p s
+-- | Escape from a @SuchThat@ by running a universally quantified continuation.
+ambiguously :: (forall s. Constrain c s => p s -> r) -> SuchThat c p -> r
+ambiguously f (SuchThat ps) = f ps
 
+-- | A @Some c@ is some value that satisfies all the constraints in @c@.
+type Some c = SuchThat c Identity
+
+-- | Build a @Some c@ from some thing that satisfies @c@.
+some :: Constrain c a => a -> Some c
+some = ambiguate . Identity
+
+-- | A @ForAny f@ is an @f@ containing some arbitrary values, meaning you can only apply operations that would work /for any/ type of value it contains.
+-- This is useful if the type parameter is phantom because it allows you to ignore it. 
+-- This is called "ghost busting" because it removes the phantom.
+--
+-- It can also be used for reading only the /structure/ of something rather than the actual values.
+-- For example, @ForAny []@ is isomorphic to both the natural numbers, and the fixed point of @"Maybe"@.
+type ForAny = SuchThat '[]
+
+-- | Because of how @SuchThat@ is set up, you can do natural transformations on it.
+-- This is especially useful if @c ~ '[]@, meaning you have a @ForAny@
+naturalTransformation :: (forall x. f x -> g x) -> SuchThat c f -> SuchThat c g
+naturalTransformation n = ambiguously (ambiguate . n)
+
+-- | If the type constructor in question is a functor, we can scope out its structure by converting every element to @()@.
+-- Note that this is actually the most general way to do this, since the type signature would otherwise be @Functor p => SuchThat c p -> (forall x. x -> a) -> p a@.
+-- That means the output type must be isomorphic to @()@ because that is the terminal object in @Hask@.
+--
+-- @
+--   discoverStructure [ambiguate "meow",ambiguate 'a',ambiguate 5] = [(),(),()]
+-- @
+discoverStructure :: Functor p => SuchThat c p -> p ()
+discoverStructure = ambiguously void
+
+-- | A @Satisfies x@ is a value that satisfies the single constraint x.
+type Satisfies x = Some '[x]
+
+-- A type level fold for applying many constraints to the same thing
 type family Constrain (c :: [k -> Constraint]) (x :: k) :: Constraint where
   Constrain (c ': '[]) x = c x
   Constrain (c ': cs) x = (c x,Constrain cs x)
   Constrain '[] x = x ~ x
-
--- An unconstrained SuchThat. Only useful for Ghost Busting (removing phantom types) and reading structure
---data ForAny (p :: k -> *) = forall s. ForAny (p s)
-type ForAny = SuchThat '[]
-
--- A type that satisfies some specific constraint only
-type Some x = SuchThatStar '[x]
-
--- "Inject" a value into a SuchThat. It becomes more ambiguous because it throws out the exact type of s
-ambiguate :: Constrain c s => p s -> SuchThat c p
-ambiguate = SuchThat
-
-ambiguously :: (forall s. Constrain c s => p s -> r) -> SuchThat c p -> r
-ambiguously f (SuchThat ps) = f ps
-
--- Escape from a SuchThat by running a continuation with the context
---onFA :: SuchThat c p -> (forall s. Constrain c s => p s -> r) -> r
---onFA (SuchThat ps) f = f ps
